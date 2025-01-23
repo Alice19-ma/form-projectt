1535a9b9298cb21d7dc3ca0c9a8256259d1f279f
Alice19-ma <easyloans1998@gmail.com>
2025-01-23 17:08:04 +0530

    updated script.js file

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-document.getElementById("myForm").addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the form from submitting the traditional way
-
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-
-    if (name && email && message) {
-        document.getElementById("responseMessage").textContent = `Thank you, ${name}! Your message has been received. We'll get back to you shortly.`;
-    } else {
-        document.getElementById("responseMessage").textContent = "Please fill out all fields before submitting.";
-    }
-});
-
-
 const { createClient } = supabase
 const supabaseUrl = 'https://gutkqbxzhakewweqjhek.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1dGtxYnh6aGFrZXd3ZXFqaGVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc2MTgyNzcsImV4cCI6MjA1MzE5NDI3N30.4e1TcXr2k6m0DkU2F6oCllWJrdkvuY5dzdg9SrhqbcQ';
